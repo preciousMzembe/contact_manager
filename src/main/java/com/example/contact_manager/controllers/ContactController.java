9b956f4c9b40c0efcfba1ea47b9a9ca2d79f5e7c
Precious Mzembe <preciousmzembe@gmail.com>
2023-07-08 22:40:34 +0200

    working on contact controller

--- src/main/java/com/example/contact_manager/controllers/ContactController.java
+++ src/main/java/com/example/contact_manager/controllers/ContactController.java
@@ -4,6 +4,8 @@ import com.example.contact_manager.dto.ContactDto;
 import com.example.contact_manager.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,27 +21,48 @@ public class ContactController {
 
     @GetMapping("all")
     public ResponseEntity<String> getContacts(){
+        String username = getUsernameFromSecurity();
         List<ContactDto> contacts = contactService.findContactsByUserId(1);
         return ResponseEntity.ok("All contacts");
     }
 
     @GetMapping("search/{name}")
-    public ResponseEntity<String> searchContact(){
-        return ResponseEntity.ok("search contact");
+    public ResponseEntity<List<ContactDto>> searchContact(@PathVariable("name") String name){
+        String username = getUsernameFromSecurity();
+        List<ContactDto> contacts = contactService.searchContact(1, name);
+        return ResponseEntity.ok(contacts);
     }
 
     @PostMapping("/create")
-    public ResponseEntity<String> createContact(){
-        return ResponseEntity.ok("contact created");
+    public ResponseEntity<ContactDto> createContact(@RequestBody ContactDto contactDto){
+        String username = getUsernameFromSecurity();
+        ContactDto newContact = contactService.createContact(1, contactDto);
+        return ResponseEntity.ok(newContact);
     }
 
     @PutMapping("/update/{contactId}")
-    public ResponseEntity<String> updateContact(){
-        return ResponseEntity.ok("contact updated");
+    public ResponseEntity<ContactDto> updateContact(@RequestBody ContactDto contactDto, @PathVariable("contactId") long contactId){
+        String username = getUsernameFromSecurity();
+        ContactDto updatedContact = contactService.updateContact(1, contactId, contactDto);
+        return ResponseEntity.ok(updatedContact);
     }
 
     @DeleteMapping("delete/{contactId}")
-    public ResponseEntity<String> deleteContact(){
+    public ResponseEntity<String> deleteContact(@PathVariable("contactId") long contactId){
+        String username = getUsernameFromSecurity();
+        contactService.deleteContact(1, contactId);
         return ResponseEntity.ok("contact deleted");
+    }
+
+    private String getUsernameFromSecurity(){
+        Object principle = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if(principle instanceof UserDetails){
+            username = ((UserDetails) principle).getUsername();
+        }else {
+            username = principle.toString();
+        }
+
+        return username;
     }
 }
